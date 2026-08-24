9f75c4d47b0c9c33be654deeec34fae91b99d867
Tww <2601759251@qq.com>
2026-08-24 19:12:37 +0800

    Update HUDBackdropLabel.h

--- sources/HUDBackdropLabel.h
+++ sources/HUDBackdropLabel.h
@@ -11,6 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface HUDBackdropLabel : UILabel
 - (void)setColorInvertEnabled:(BOOL)colorInvertEnabled;
+- (void)setBackdropBlurRadius:(CGFloat)blurRadius contrastAmount:(CGFloat)contrastAmount;
 @end
 
 NS_ASSUME_NONNULL_END
